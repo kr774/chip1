35f0919a8519ba12bc599e81c3528b361ca96adf
Joe <joemmalatesta@gmail.com>
2025-04-15 16:27:41 -0400

    add decryption.

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,12 +1,20 @@
 // @ts-nocheck
 import { redirect } from '@sveltejs/kit';
-
+import { SECRET_KEY, IV } from '$env/static/private';
+import crypto from 'crypto';
 
 /** @type {import('./$types').LayoutServerLoad} */
 // @ts-ignore
 export async function load({ request, url, locals }) {
 	const code = url.searchParams.get('code');
 	const userAgent = request.headers.get('user-agent');
+	let decrypted = '';
+	if (code) {
+		const decipher = crypto.createDecipheriv('aes-256-cbc', SECRET_KEY.slice(0, 32), IV);
+		decrypted = decipher.update(code, 'hex', 'utf8');
+		decrypted += decipher.final('utf8');
+		console.log('Decrypted code:', decrypted);
+	}
 
 	if (!locals.user) {
 		console.log(`Requested code ${code}`);
@@ -18,15 +26,15 @@ export async function load({ request, url, locals }) {
 		const iosRegex = /mac|iPhone/i; 
 		if (iosRegex.test(userAgent)) { 
 			console.log('Redirecting for iOS');
-			redirect(307, `sms:888222&body=${code}`);
+			redirect(307, `sms:888222&body=${decrypted}`);
 		}
 		// Android or other platforms
 		else {
 			console.log('Sending SMS link for NOT iPhone');
 			const code = url.searchParams.get('code');
-			const href = `sms:888222;?&body=${code}`;
+			const href = `sms:888222;?&body=${decrypted}`;
 
-			redirect(307, `sms:888222;?&body=${code}`);
+			redirect(307, `sms:888222;?&body=${decrypted}`);
 		};
 
 
